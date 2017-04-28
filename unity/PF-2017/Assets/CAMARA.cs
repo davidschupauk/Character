c2d25cab99dbbf729ec3abdb90cafbc1f439d8c6
DAVID SCHUPAUK <42368931@ort>
2017-04-28 11:16:30 -0300

    comment

--- unity/PF-2017/Assets/CAMARA.cs
+++ unity/PF-2017/Assets/CAMARA.cs
@@ -5,22 +5,18 @@ public class CAMARA : MonoBehaviour {
     Rigidbody body;
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 15F;
-    public float sensitivityY = 15F;
+    public float speed = 1.0f;
+    public float sensitivityX = 0.2F;
+    public float sensitivityY = 0.2f;
     public float minimumX = -360F;
     public float maximumX = 360F;
     public float minimumY = -60F;
     public float maximumY = 60F;
     float rotationY = 0F;
-    // Use this for initialization
-    void Start () {
-        // Make the rigid body not change rotation
-        body.freezeRotation = true;
 
-    }
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update() {
+
         if (axes == RotationAxes.MouseXAndY)
         {
             float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
@@ -41,5 +37,7 @@ public class CAMARA : MonoBehaviour {
 
             transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
         }
+        
     }
+
 }
